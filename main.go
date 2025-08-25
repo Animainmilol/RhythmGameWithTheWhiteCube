9fcf9413749d54cbb78048515490b3d74951e463
Animainmilol <animainmilol@gmail.com>
2025-08-25 14:18:22 +0500

    basics

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+const (
+	InitWindowWidth  = 800
+	InitWindowHeight = 450
+	TargetFPS        = 1024
+)
+
+func main() {
+	rl.InitWindow(InitWindowWidth, InitWindowHeight, "RhythmGameWithTheWhiteCube")
+	rl.SetWindowState(rl.FlagWindowResizable)
+	defer rl.CloseWindow()
+	rl.SetTargetFPS(TargetFPS)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.Black)
+
+		rl.EndDrawing()
+	}
+}
