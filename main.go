9f440608cfc103cc88fe778819bccc6e2159c165
Animainmilol <animainmilol@gmail.com>
2025-08-27 10:13:10 +0500

    camera

--- main.go
+++ main.go
@@ -16,12 +16,19 @@ func main() {
 	defer rl.CloseWindow()
 	rl.SetTargetFPS(TargetFPS)
 
+	cameraController := NewCameraController()
+
 	for !rl.WindowShouldClose() {
+		cameraController.Update()
+		cameraController.HandleInput()
+
 		rl.BeginDrawing()
+		rl.BeginMode2D(cameraController.Camera)
 
 		rl.ClearBackground(rl.Black)
 		rl.DrawRectangle(0, 0, 100, 100, rl.RayWhite)
 
+		rl.EndMode2D()
 		rl.EndDrawing()
 	}
 }
